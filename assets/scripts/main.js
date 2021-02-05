3224eeb55a880d268305b18bf8fbd4f5ad1c5969
xlmentx <48296282+xlmentx@users.noreply.github.com>
2021-02-05 13:48:12 -0800

    Update main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -4,6 +4,6 @@ var snd = document.getElementById("horn-sound");
 
 var val = getRadioVal(document.getElementById('audio-selection'), 'radio-sound');
 
-button.addEventListener('click', function(){snd.play()}); 
-button.addEventListener('click', function(){alert(val)}); 
+btn.addEventListener('click', function(){snd.play()}); 
+tBTN.addEventListener('click', function(){alert(val)}); 
 
