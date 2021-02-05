9c31aa1edbcb502e01ba0ae16fd208289a167563
xlmentx <48296282+xlmentx@users.noreply.github.com>
2021-02-05 14:46:50 -0800

    Update main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,9 +1,19 @@
-var btn = document.getElementById("honk-btn");
-var tBTN = document.getElementById("test");
-var snd = document.getElementById("horn-sound");
+var button = document.getElementById("honk-btn");
+var airhorn = new Audio("./assets/media/audio/air-horn.mp3");
+var carhorn = new Audio("./assets/media/audio/car-horn.mp3");
+var partyhorn = new Audio("./assets/media/audio/party-horn.mp3");
 
-var val = getRadioVal(document.getElementById("audio-selection"), "radio-sound");
-alert("Check box not selected");
-btn.addEventListener('click', snd.play()); 
-tBTN.addEventListener('click', alert(val)); 
-
+button.addEventListener("click", function() {
+    if(document.getElementById("radio-air-horn").checked)
+    { alert("radio-air-horn");
+      airhorn.play();
+    }
+    else if(document.getElementById("radio-car-horn").checked)
+    {	alert("radio-car-horn");
+      carhorn.play();
+    }
+    else if(document.getElementById("radio-party-horn").checked)
+    {	alert("radio-party-horn");
+      partyhorn.play();
+    }
+});
