f802d4f4af9c6ac4dd1e2e0802cf5c50f8570b1b
xlmentx <48296282+xlmentx@users.noreply.github.com>
2021-02-05 15:05:53 -0800

    Update main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,10 +1,10 @@
 var button = document.getElementById("honk-btn");
 var airhorn = document.getElementById("horn-sound");
-
+var audio = new Audio('assets/media/audio/air-horn.mp3');
 button.addEventListener("click", function() {
     if(document.getElementById("radio-air-horn").checked)
     { alert("radio-air-horn");
-      airhorn.play();
+      audio.play();
     }
     else if(document.getElementById("radio-car-horn").checked)
     {	alert("radio-car-horn");
