45a04031bd3ae95df316b0b5954a2ae8524143f8
xlmentx <48296282+xlmentx@users.noreply.github.com>
2021-02-05 14:04:17 -0800

    Update main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -2,7 +2,7 @@ var btn = document.getElementById("honk-btn");
 var tBTN = document.getElementById("test");
 var snd = document.getElementById("horn-sound");
 
-var val = getRadioVal(document.getElementById('audio-selection'), 'radio-sound');
+var val = getRadioVal(document.getElementById("audio-selection"), "radio-sound");
 
 btn.addEventListener('click', snd.play()); 
 tBTN.addEventListener('click', alert(val)); 
