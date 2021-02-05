7b46de64d9bf9870d7b0c630e04f7d161dc8d0df
xlmentx <48296282+xlmentx@users.noreply.github.com>
2021-02-05 15:27:17 -0800

    Update main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -3,8 +3,8 @@ var airhorn = document.getElementById("horn-sound");
 var audio = new Audio('assets/media/audio/party-horn.mp3');
 button.addEventListener("click", function() {
     if(document.getElementById("radio-air-horn").checked)
-    { 
-      airhorn.play();
+    {   function(){audio.play();} 
+
     }
     else if(document.getElementById("radio-car-horn").checked)
     {	alert("radio-car-horn");
