6673174ed6e0f24ffa7cda3498ddbd8433f5bd73
xlmentx <48296282+xlmentx@users.noreply.github.com>
2021-02-05 12:53:38 -0800

    Update main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -2,3 +2,6 @@ var btn = document.getElementById("honk-btn");
 var snd = document.getElementById("horn-sound");
 
 button.addEventListener('click', snd.play()); 
+
+
+var sound = new Audio("./assets/media/audio/air-horn.mp3"); 
