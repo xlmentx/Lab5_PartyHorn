9121ef541d54dd8abab2ced16ff615a96e298f2a
xlmentx <48296282+xlmentx@users.noreply.github.com>
2021-02-05 16:14:06 -0800

    Update main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -6,17 +6,17 @@ var sound = new Audio('assets/media/audio/air-horn.mp3');
 var button = document.getElementById("honk-btn");
 
 airhorn.addEventListener("click", function(){
-    image = new Image('assets/media/images/air-horn.svg');
+    image.src = "assets/media/images/air-horn.svg";
     sound = new Audio('assets/media/audio/air-horn.mp3');
 });
     
 carhorn.addEventListener("click", function(){
-    image = new Image('assets/media/images/car.svg');
+    image.src = "assets/media/images/car.svg";
     sound = new Audio('assets/media/audio/car-horn.mp3');
 });
 
 partyhorn.addEventListener("click", function(){
-    image = new Image('assets/media/images/party-horn.svg');
+    image.src = "assets/media/images/party-horn.svg";
     sound = new Audio('assets/media/audio/party-horn.mp3');
 });
 
