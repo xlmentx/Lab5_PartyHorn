a28b79b64337bb077a7dbf3cce42ba065a0a1e03
xlmentx <48296282+xlmentx@users.noreply.github.com>
2021-02-05 16:43:24 -0800

    Update main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,3 +1,4 @@
+var volumeImage = document.getElementById("volume-image");
 var volumeNumber = document.getElementById("volume-number");
 var volumeSlider = document.getElementById("volume-slider");
 var airhorn = document.getElementById("radio-air-horn");
@@ -7,10 +8,30 @@ var image = document.getElementById("sound-image");
 var sound = new Audio('assets/media/audio/air-horn.mp3');
 var button = document.getElementById("honk-btn");
 
+volumeNumber.addEventListener("change", function(){
+    volumeSlider.value = volumeNumber.value;
+    updateVolumeIcon();
+});
 
 volumeSlider.addEventListener("change", function(){
     volumeNumber.value = volumeSlider.value;
+    updateVolumeIcon();
 });
+
+function updateVolumeIcon()
+{   if(volumeNumber.value == 0)
+    {   volumeImage.src = "assets/media/icons/volume-level-0.svg";
+    }   
+    else if(volumeNumber.value <= 33)
+    {   volumeImage.src = "assets/media/icons/volume-level-1.svg";
+    }   
+    else if(volumeNumber.value <= 66)
+    {   volumeImage.src = "assets/media/icons/volume-level-2.svg";
+    }   
+    else
+    {   volumeImage.src = "assets/media/icons/volume-level-3.svg";
+    }   
+}
 
 airhorn.addEventListener("click", function(){
     image.src = "assets/media/images/air-horn.svg";
@@ -29,11 +50,6 @@ partyhorn.addEventListener("click", function(){
 
 button.addEventListener("click", function(event) {
     event.preventDefault();
-    sound.volume = getNumber();
+    sound.volume = volumeNumber.value;
     sound.play(); 
 });
-
-function getNumber(){
-    alert(volumeNumber.value);
-    return volumeNumber.value; 
-}
