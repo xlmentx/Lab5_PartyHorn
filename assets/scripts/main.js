d4a4aa8336e77ef5d9fbec1755f553b765ce33cb
xlmentx <48296282+xlmentx@users.noreply.github.com>
2021-02-05 16:30:34 -0800

    Update main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,9 +1,16 @@
+var volumeNumber = document.getElementById("volume-number");
+var volumeSlider = document.getElementById("volume-slider");
 var airhorn = document.getElementById("radio-air-horn");
 var carhorn = document.getElementById("radio-car-horn");
 var partyhorn = document.getElementById("radio-party-horn");
 var image = document.getElementById("sound-image");
 var sound = new Audio('assets/media/audio/air-horn.mp3');
 var button = document.getElementById("honk-btn");
+
+
+volumeSlider.addEventListener("change", function(){
+    volumeNumber.value = volumeSlider.value;
+});
 
 airhorn.addEventListener("click", function(){
     image.src = "assets/media/images/air-horn.svg";
@@ -20,8 +27,13 @@ partyhorn.addEventListener("click", function(){
     sound = new Audio('assets/media/audio/party-horn.mp3');
 });
 
-
 button.addEventListener("click", function(event) {
     event.preventDefault();
+    sound.volume = getNumber();
     sound.play(); 
 });
+
+function getNumber(){
+    alert(volumeNumber.value);
+    return volumeNumber.value; 
+}
